df34ba613fa7a9b2d780966b5a1e3c76533b29b7
Rafikhan15 <mdrafi.khan0089@gmail.com>
2022-07-01 10:45:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = "mongodb+srv://admin:admin@cluster0.x72yl.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const taskCollection = client.db('task-manager').collection('task');
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
